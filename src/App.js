eae086771223e0834bff9d8ac701b1a5f59e4a6e
Kevin Pham <kevin.pham1@vmogroup.com>
2024-07-18 16:20:08 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,57 +1,58 @@
-import {useState} from "react";
-
-function Square({value, onSquareClick}) {
-    return <button className="square" onClick={onSquareClick}>
-        {value}
-    </button>
+function ProductCategoryRow({category}) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    )
 }
 
-export default function Board() {
-    const [squares, setSquares] = useState(Array(9).fill(null))
+function ProductRow({product}) {
+    const name = product.stocked ? product.name : <span style={{color: 'red'}}>{product.name}</span>
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    )
+}
 
-    function handleClick(i) {
-        const nextSquares = squares.slice();
-        nextSquares[i] = "X";
-        // Set state again will re-render components and child components  that using state
-        setSquares(nextSquares);
+function ProductTable({products}) {
+    const rows = [];
+    let lastCategory = null;
 
-        // onSquareClick={handleClick(0)}/> will infinite loop, because it is a call function, function
-        // is executed -> change state 0 -> re-rendering component -> call handleClick(0) again, so on
-        // -> to fix this, use callback function
-
-        // Parent component pass props to child component
-        // Parent component handle state
-
-        // Now that your state handling is in the Board component,
-        // the parent Board component passes props to the child Square components so that they can be displayed correctly.
-        // When clicking on a Square, the child Square component now asks the parent Board component to update the state of the board.
-        // When the Board’s state changes, both the Board component and every child Square re-renders automatically.
-        // Keeping the state of all squares in the Board component will allow it to determine the winner in the future.
-
-        // For convention, use onSomething for props represent events and handleSomething for handle those events
-
-        // Immutability make complex features to easier implement, ex undo / redo function, should mutate state directly
-        //, make to compare data change or not easier for performance reasons
-    }
+    products.forEach((product) => {
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category}
+                />
+            );
+        }
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name}
+            />
+        );
+        lastCategory = product.category;
+    });
 
     return (
-        <>
-            <div className="board-row">
-
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
-        </>
+        <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
     )
+}
+
+function SearchBar() {
+
 }
