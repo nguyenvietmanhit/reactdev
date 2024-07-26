827d33aeb2a2369dd26926211dd02c0bc6e9423d
Kevin Pham <kevin.pham1@vmogroup.com>
2024-07-26 17:31:24 +0700

    a

--- src/App.js
+++ src/App.js
@@ -54,5 +54,43 @@ function ProductTable({products}) {
 }
 
 function SearchBar() {
-
+    return (
+        <form>
+            <input type="text" placeholder="Search..." />
+            <br />
+            <label>
+                <input type="checkbox"/>
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    )
 }
+
+function FilterableProductTable({ products }) {
+    return (
+        <div>
+            <SearchBar />
+            <ProductTable products={products} />
+        </div>
+    )
+}
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+]
+
+export default function App() {
+    return <FilterableProductTable products={PRODUCTS} />
+}
+
+// Which of these are state? Identify the ones that are not:
+//
+//     Does it remain unchanged over time? If so, it isn’t state.
+//     Is it passed in from a parent via props? If so, it isn’t state.
+//     Can you compute it based on existing state or props in your component? If so, it definitely isn’t state!
